3de728b665164751889157bf30026f38918b7f54
royap <royaparsa294@gmail.com>
2023-06-25 11:07:59 -0400

    made popup permanent

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,20 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const Popup = () => {
-  const [showPopup, setShowPopup] = useState(true);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShowPopup(false);
-    }, 5000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
-  if (!showPopup) {
-    return null;
-  }
-
   const handleLinkedInClick = () => {
     window.open('https://www.linkedin.com/in/christopher-nielsen-ab6233205/', '_blank');
   };
@@ -22,12 +8,12 @@ const Popup = () => {
   return (
     <div className="fixed bottom-4 right-4 z-50">
       <div className="bg-white rounded-lg p-8 shadow-lg">
-        <p className="text-lg text-center mb-4">Click here to connect with me on Linkedin!</p>
+        <p className="text-lg text-center mb-4">Click here to connect with me on LinkedIn!</p>
         <button
           className="bg-green-500 hover:bg-green-600 text-white rounded-lg px-4 py-2 text-lg"
           onClick={handleLinkedInClick}
         >
-          Visit Linkedin
+          Visit LinkedIn
         </button>
       </div>
     </div>
