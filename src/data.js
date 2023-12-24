458407c5bcac7c2ae3b0e83eaef489e999f26f15
roya parsa <99053997+rparsa49@users.noreply.github.com>
2023-12-24 12:01:04 -0500

    fall 2023 updates

--- src/data.js
+++ src/data.js
@@ -1,30 +1,42 @@
 export const projects = [
-    {
-        title: "Lidar-Guided Automated Driving Project",
-        subtitle: "C, PetaLinux, XILINX SDK 20127.2, Tera Term",
-        description: "The primary objective was to explore its potential in autonomous driving systems, a domain currently gaining considerable attention. Leveraging available modules specialized in data handling and power generation, the project aimed to achieve feasible and autonomous operations.",
-        image: process.env.PUBLIC_URL + "/75ARM1_conduction_cooled.png",
-        link: "https://github.com/CypherVault/NAI-75ARM1-SelfNavigationProgram"
-    },
-    {
-        title: "CMOS 45nm D-FlipFlop",
-        subtitle: "VSLI, Cadence",
-        description: "Built and Simulated a CMOS 45nm D-FlipFlop, Based on the free 45nmPDK Library Suite from Cadence. Dissimilation an optimization of NMOS and PMOS sizing for cleaner signal, symmetrica fallrise and delay.",
-        image: process.env.PUBLIC_URL + "/project2.png",
-        link: "https://rparsa49.github.io/chris-portfolio/DFF%20Report.pdf"
-    },
-    {
-        title: "Library Management System",
-        subtitle: "C, C++",
-        description: "Project required the creation of a C++ program to run a digital library. Users must login with valid credentials, and are able to borrow, return, renew, and search for books within the library database. Teacher logins are able to remove books from the library, and request new books to be added to the database. This project stretched the limit of what our group could implement given our tools. However by using complex data structures, verification programs, loop based flows, and object oriented programming, we implemented all required functions of the project assignment.",
-        image: process.env.PUBLIC_URL + "/project3.png",
-        link: "https://github.com/goonmandu/sbu_fall2022_ese224/tree/main/projects"
-    },
-    {
-        title: "AI Maze Solver",
-        subtitle: "C",
-        description: "Ai which can traverse a “matrix” based maze, with the goal of determining a rule of where higher point values are stored: Low level AI reasoning was achieved within C.",
-        image: process.env.PUBLIC_URL + "/project1.png",
-        link: "https://github.com/CypherVault/ESE124_Lab-HW_Docs",
-    },
+  {
+    title: "SIMD PROCESSOR DESIGN PROJECT",
+    subtitle: "VHDL, CAD",
+    description:
+      "Junior year VHDL hardware description language + modern CAD tool project of the structural and behavioral design of a four-stage pipelined multimedia unit with a reduced set of multimedia instructions similar to those in the Sony Cell SPU and Intel SSE architectures.",
+    image: process.env.PUBLIC_URL + "/SMID.jpeg",
+    link: "https://github.com/CypherVault/ESE-345-PMU-PROJECT",
+  },
+  {
+    title: "Lidar-Guided Automated Driving Project",
+    subtitle: "C, PetaLinux, XILINX SDK 20127.2, Tera Term",
+    description:
+      "The primary objective was to explore its potential in autonomous driving systems, a domain currently gaining considerable attention. Leveraging available modules specialized in data handling and power generation, the project aimed to achieve feasible and autonomous operations.",
+    image: process.env.PUBLIC_URL + "/75ARM1_conduction_cooled.png",
+    link: "https://github.com/CypherVault/NAI-75ARM1-SelfNavigationProgram",
+  },
+  {
+    title: "CMOS 45nm D-FlipFlop",
+    subtitle: "VSLI, Cadence",
+    description:
+      "Built and Simulated a CMOS 45nm D-FlipFlop, Based on the free 45nmPDK Library Suite from Cadence. Dissimilation an optimization of NMOS and PMOS sizing for cleaner signal, symmetrica fallrise and delay.",
+    image: process.env.PUBLIC_URL + "/project2.png",
+    link: "https://rparsa49.github.io/chris-portfolio/DFF%20Report.pdf",
+  },
+  {
+    title: "Library Management System",
+    subtitle: "C, C++",
+    description:
+      "Project required the creation of a C++ program to run a digital library. Users must login with valid credentials, and are able to borrow, return, renew, and search for books within the library database. Teacher logins are able to remove books from the library, and request new books to be added to the database. This project stretched the limit of what our group could implement given our tools. However by using complex data structures, verification programs, loop based flows, and object oriented programming, we implemented all required functions of the project assignment.",
+    image: process.env.PUBLIC_URL + "/project3.png",
+    link: "https://github.com/goonmandu/sbu_fall2022_ese224/tree/main/projects",
+  },
+  {
+    title: "AI Maze Solver",
+    subtitle: "C",
+    description:
+      "Ai which can traverse a “matrix” based maze, with the goal of determining a rule of where higher point values are stored: Low level AI reasoning was achieved within C.",
+    image: process.env.PUBLIC_URL + "/project1.png",
+    link: "https://github.com/CypherVault/ESE124_Lab-HW_Docs",
+  },
 ];
